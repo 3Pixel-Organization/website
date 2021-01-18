0e75946e75d4cdb8d82f28484a953617a95c9ff9
Joaquim Neto <joaquimmy@yahoo.com>
2021-01-18 19:39:50 -0300

    feat: add video to home page

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,17 +3,17 @@ import { Container } from '../container/Container';
 
 import classes from './Hero.module.css';
 
-export const Hero = ({ title, subtitle, image }) => {
+export const Hero = ({ title, subtitle, image, cta }) => {
   return (
-    <Container
-      className={classes.Hero}
-      justifyCenter
-      alignCenter
-      fluid
-    >
-      <div className={classes.BackgroundBlur} style={{ backgroundImage: `url(${image})` }} aria-hidden={true}></div>
+    <Container className={classes.Hero} justifyCenter alignCenter fluid>
+      <div
+        className={classes.BackgroundBlur}
+        style={{ backgroundImage: `url(${image})` }}
+        aria-hidden={true}
+      ></div>
       <h1 className={classes.Title}>{title}</h1>
       <p className={classes.Subtitle}>{subtitle}</p>
+      {cta}
     </Container>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,17 +4,22 @@ import { Container } from '../../components/container/Container';
 import { Hero } from '../../components/hero/Hero';
 import { Card } from '../../components/card/Card';
 import HeroImage from '../../assets/hero.jpg';
+import { Video } from './Video';
 
 export const Home = () => {
   return (
     <>
       <Hero title='Team 3Pixel' subtitle='We make games.' image={HeroImage} />
       <Container justifyCenter column fadeIn>
-        <Card style={{margin: '2rem 0'}}>
+        <Card style={{ margin: '2rem 0' }}>
           <h1>🚧 Under Construction</h1>
           <hr />
-          <p>We're working hard to get this website up and running.</p>
-          <p>Keep tunned for upcoming 3Pixel news.</p>
+          <Container justifyCenter alignCenter fluid>
+            <p>We're working hard to get this website up and running.</p>
+            <p>Keep tunned for upcoming 3Pixel news.</p>
+            <br />
+            <Video embed='https://www.youtube.com/embed/ce7gTqGik2s' />
+          </Container>
         </Card>
       </Container>
     </>
